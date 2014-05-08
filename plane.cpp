a7e87ca5c7b0d9d621ae1ddf7cf73af94c97d784
DiegoMarcia <gpimple@gmail.com>
2014-05-08 22:24:31 +0200

    Documentato il commit precedente.

--- plane.cpp
+++ plane.cpp
@@ -19,6 +19,13 @@ Plane::Plane(QVector<GeomNode> readNodes, double **readDistances, bool instanceT
     }
 }
 
+/**
+ * @brief Distanza tra due nodi
+ *
+ * Dati gli ID di due nodi, rende la distanza euclidea tra essi.
+ *
+ * @author Diego Marcia { gpimple@gmail.com }
+ */
 double Plane::distance (int nodeA, int nodeB){
     if (this->symmetricInstance) {
 
@@ -31,6 +38,13 @@ double Plane::distance (int nodeA, int nodeB){
     }
 }
 
+/**
+ * @brief Distanza tra due nodi al quadrato
+ *
+ * Dati gli ID di due nodi, rende la distanza al quadrato. Utile per confrontare più distanze senza dover ricorrere alla funzione sqrt.
+ *
+ * @author Diego Marcia { gpimple@gmail.com }
+ */
 double Plane::squared_distance(int nodeA, int nodeB){
     double x_dist = nodes.at(nodeA).get_x_coord() - nodes.at(nodeB).get_x_coord();
     double y_dist = nodes.at(nodeA).get_y_coord() - nodes.at(nodeB).get_y_coord();
@@ -38,16 +52,23 @@ double Plane::squared_distance(int nodeA, int nodeB){
     return (x_dist * x_dist) + (y_dist * y_dist);
 }
 
+/**
+ * @brief Nodo a distanza minore.
+ *
+ * Dato l'ID di un nodo, rende l'ID del nodo più vicino e la distanza tra essi.
+ *
+ * @author Diego Marcia { gpimple@gmail.com }
+ */
 QPair<int, double> Plane::closest (int node){
     QPair<int, double> min; // Il nodo più vicino e la distanza da quello ricevuto.
 
-    int minPos=0;
-    double minDist;     // Assumo che il più vicino sia il primo dei nodi attivi
+    int minPos=0;     // Assumo che il più vicino sia il primo dei nodi attivi
+    double minDist;
     double calcDist;
 
     if (this->symmetricInstance){
         minDist = this->squared_distance(node, this->activeNodes.at(0));
-    } else {
+    } else {    // Caso asimmetrico
         minDist = distances[node][0];
     }
 
@@ -74,11 +95,18 @@ QPair<int, double> Plane::closest (int node){
     return min;
 }
 
+/**
+ * @brief Recupera un nodo
+ *
+ * Dato l'ID di un nodo, rende l'oggetto GeomNode associato.
+ *
+ * @author Diego Marcia { gpimple@gmail.com }
+ */
 GeomNode Plane::get_node(int whichNode){
     if ((whichNode >-1) && (whichNode < this->nodes.size()))
         return this->nodes.at(whichNode);
     else {
         qCritical("Richiesto un nodo inesistente! %d", whichNode);
-        return GeomNode(-1, -1, -1);
+        return GeomNode(-1, -1, -1);    // Trovare un altro modo che non implichi eccezioni e non sia ambiguo.
     }
 }
